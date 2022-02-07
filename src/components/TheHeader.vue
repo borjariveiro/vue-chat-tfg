866018dd1ff2615e261203ad69c2d5bed9e568d2
Borja Riveiro <borja.riveiro@hotmail.com>
2022-02-07 17:11:33 +0100

    feat: Add toast when logout

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -18,16 +18,20 @@
 import { computed } from 'vue'
 import { useUserStore } from '../stores/user'
 import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
 
 const userStore = useUserStore()
 const router = useRouter()
+const toast = useToast()
 const user = computed(() => userStore.user)
 
 async function doLogout() {
   try {
     await userStore.doLogout()
+    toast.success('Sesion cerrada')
     router.push({ name: 'login' })
   } catch (error) {
+    toast.error('Error al cerrar sesion')
     console.log(error.message)
   }
 }
