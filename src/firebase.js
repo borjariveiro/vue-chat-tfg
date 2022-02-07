53e8efcd76f90a27f42a13338ad62df0b5fb1022
Borja Riveiro <borja.riveiro@hotmail.com>
2022-02-07 16:03:32 +0100

    feat: Setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAMGXRhBg47v5HGy2Ezy5RA88oIz73FN2k',
+  authDomain: 'vue-chat-tfg.firebaseapp.com',
+  projectId: 'vue-chat-tfg',
+  storageBucket: 'vue-chat-tfg.appspot.com',
+  messagingSenderId: '760613494781',
+  appId: '1:760613494781:web:79f81087f2abd2e04d0394'
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export { app, auth }
