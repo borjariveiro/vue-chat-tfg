193be5f156f37faa492e760be625c4f4a7a39577
Borja Riveiro <borja.riveiro@hotmail.com>
2022-02-07 16:59:49 +0100

    feat: Add TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <template>
-  <h1>Vue Chat</h1>
+  <TheHeader />
   <RouterView />
 </template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="flex items-center justify-between">
+    <div>
+      <router-link class="m-4 p-2 border-2 border-green-700" to="/login"
+        >Sign in</router-link
+      >
+      <router-link class="m-4 p-2 border-2 border-green-700" to="/register"
+        >Sign up</router-link
+      >
+      <button @click="doLogout" class="border-2 px-2 py-1">Logout</button>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { useUserStore } from '../stores/user'
+import { useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+const router = useRouter()
+
+async function doLogout() {
+  try {
+    await userStore.doLogout()
+    router.push({ name: 'login' })
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+</script>
