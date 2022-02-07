6e32b1860d7455a8be30b5cb5cbfb7df1a09f22a
Borja Riveiro <borja.riveiro@hotmail.com>
2022-02-07 17:03:06 +0100

    feat: Add if statement in logout button

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,17 +7,21 @@
       <router-link class="m-4 p-2 border-2 border-green-700" to="/register"
         >Sign up</router-link
       >
-      <button @click="doLogout" class="border-2 px-2 py-1">Logout</button>
+      <button v-if="user" @click="doLogout" class="border-2 px-2 py-1">
+        Logout
+      </button>
     </div>
   </header>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useUserStore } from '../stores/user'
 import { useRouter } from 'vue-router'
 
 const userStore = useUserStore()
 const router = useRouter()
+const user = computed(() => userStore.user)
 
 async function doLogout() {
   try {
